e3d70eea8e01a0de0c8443d73ece162eb144232a
Damian Esteban <damian.esteban@gmail.com>
2021-07-20 16:08:17 -0400

    Add google test

--- cypress/integration/app-spec.ts
+++ cypress/integration/app-spec.ts
@@ -1,7 +1,14 @@
 // type definitions for Cypress object "cy"
 /// <reference types="cypress" />
 
+describe('Google', function() {
+  it('Successfully loads Google.com', () => {
+    cy.visit('https://www.google.com') 
+});
+
+})
 describe("App", function() {
+
   beforeEach(function() {
     cy.visit("/");
   });
