9aa5fb654882c55c3e48b0681ebd19814992cc30
Damian Esteban <damian.esteban@gmail.com>
2021-07-20 10:45:07 -0400

    Test ci

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -8,6 +8,7 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
+
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
